3e8ed7eb591d4b6b3daefc5fd6d99f04c62c2b9f
rarebit3 <johntrevorbond@gmail.com>
2022-04-05 17:17:18 -0400

    some more front end built

--- client/src/components/BlogPosts.js
+++ client/src/components/BlogPosts.js
@@ -1,18 +1,39 @@
+import React, { useEffect } from 'react'
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { LoadBlogPosts } from "../store/actions/BlogPostActions";
 import Comments from './Comments'
 import Likes from './Likes'
 
-const BlogPosts = () => {
+const mapStateToProps = ({ blogState }) => {
+    return { blogState };
+  };
+  
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      fetchBlogPosts: () => dispatch(LoadBlogPosts()),
+    };
+  };
+
+const BlogPosts = (props) => {
     
+    useEffect(() => {
+        props.fetchBlogPosts()
+      }, [])
     
     return (
         <div className="blog-post-grid">
-            <h2 className='location-title'>mapped blog posts</h2>
-            <h3>Country:</h3>
-            <image />
+            {props.blogState.blogPosts.map((blogPost) => (
+            <ul className='location-title' key={blogPost.id}>
+            <h3>Country: {blogPost.country}</h3>
+            <h3>City: {blogPost.city}</h3>
+            <image src={`${blogPost.image}`} />
             <Likes />
             <Comments />
+            </ul> 
+            ))}
         </div>
     )
 }
 
-export default BlogPosts
+export default connect (mapStateToProps, mapDispatchToProps) (BlogPosts)
--- client/src/components/Likes.js
+++ client/src/components/Likes.js
@@ -1,11 +1,22 @@
 
-
+// broken -- convert from useState to redux
 const Likes = () => {
-    return (
-        <div>
-            <h6>Like:</h6>
-        </div>
-    )
-}
+    // const addALike = () => {
+    //     setTotalLikes(totalLikes + 1)
+    //   }
+  
+    //   const removeALike = () => {
+    //       if (totalLikes === 0) {
+  
+    //       } else setTotalLikes(totalLikes - 1)
+    //   }
+  
+      return (
+          <div className='likes'>
+              {/* <button onClick={addALike}>+{totalLikes}</button>
+              <button onClick={removeALike}>-{totalLikes}</button> */}
+          </div>
+      )
+  }
 
 export default Likes
